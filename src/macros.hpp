0036a6cd851e85ebfbc60ed583d933bc7df396fc
Eli <78963198+niceEli@users.noreply.github.com>
2024-09-20 12:58:05 -0400

    feat: let and var

---
+++ src/macros.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#pragma region Macros
+
+// javascript style C++
+#define let auto
+
+// swift style c++
+#define var const auto
+
+#pragma endregion
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,12 @@
 #include <iostream>
 #include <stdexcept>
 #include <fmt/core.h>
+#include <string>
+#include "macros.hpp"
+#include <string>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     fmt::println("Hello, World!");
+
+    return EXIT_SUCCESS;
 }
